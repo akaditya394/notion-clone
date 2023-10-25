487e65c9131713cd06e952a96b6362a8aa71aad0
Aditya <akaditya394@gmail.com>
2023-10-25 23:29:03 +0530

    banner component

--- app/(main)/_components/trash-box.tsx
+++ app/(main)/_components/trash-box.tsx
@@ -24,7 +24,7 @@ const TrashBox = () => {
   });
 
   const onClick = (documentId: string) => {
-    router.push(`documents/${documentId}`);
+    router.push(`/documents/${documentId}`);
   };
 
   const onRestore = (
